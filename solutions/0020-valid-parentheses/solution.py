7213083a5e1cb92db4ebb81f75b789a0dbf4aee3
Mubashir Zamir <79789067+mubashirzamir@users.noreply.github.com>
2023-02-19 23:30:31 +0000

    [LeetCode Sync] Runtime - 34 ms (4.01%), Memory - 13.8 MB (100.00%)

---
+++ solutions/0020-valid-parentheses/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for c in s:
+            if (c == '(' or c == '{' or c == '['): stack.append(c)
+            elif (len(stack) == 0): return False    
+            elif (c == ')' and stack[-1] == '('): stack.pop()
+            elif (c == '}' and stack[-1] == '{'): stack.pop()
+            elif (c == ']' and stack[-1] == '['): stack.pop()
+            else: return False
+        if (len(stack) == 0): return True
+        else : return False                    
+                    
