62d62a3724898248536b55e9028751aff74c49fd
Mubashir Zamir <79789067+mubashirzamir@users.noreply.github.com>
2023-07-15 23:27:16 +0000

    [LeetCode Sync] Runtime - 123 ms (5.01%), Memory - 53.4 MB (100.00%)

---
+++ solutions/0049-group-anagrams/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const map = new Map();
+    for (let str of strs) {
+        const sorted = [...str].sort().join("");
+        if (map.has(sorted)) {
+            map.get(sorted).push(str);
+        } else {
+            map.set(sorted, [str]);
+        }
+    }
+
+    return Array.from(map.values()); 
+};
