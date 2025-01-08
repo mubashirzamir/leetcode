39b9c78ab80e4577f9a0e01d8e3b068b4c9c0953
Mubashir Zamir <79789067+mubashirzamir@users.noreply.github.com>
2025-01-08 22:52:47 +0000

    [LeetCode Sync] Runtime - 4 ms (58.75%), Memory - 51.5 MB (55.16%)

--- solutions/0001-two-sum/solution.js
+++ solutions/0001-two-sum/solution.js
@@ -5,9 +5,9 @@
  */
 var twoSum = function(nums, target) {
     let map = {}
-    for (let i = 0; i < nums.length; i++) { 
-        if (map.hasOwnProperty([target - nums[i]])) {
-            return [map[target- nums[i]], i]
+    for (let i = 0; i < nums.length; i++) {
+        if (map.hasOwnProperty(target - nums[i])) {
+            return [map[target - nums[i]], i]
         }
         map[nums[i]] = i
     }
