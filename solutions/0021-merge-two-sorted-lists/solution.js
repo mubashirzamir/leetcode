6ffd81029532d08039f4de6b651315403a3c2eee
Mubashir Zamir <79789067+mubashirzamir@users.noreply.github.com>
2025-01-09 22:26:43 +0000

    [LeetCode Sync] Runtime - 1 ms (47.24%), Memory - 51.8 MB (74.54%)

---
+++ solutions/0021-merge-two-sorted-lists/solution.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+    let head = new ListNode()
+    let list3 = head
+
+    while (list1 && list2) {
+
+        if (list1.val < list2.val) {
+            list3.next = list1
+            list1 = list1.next
+        } else {
+            list3.next = list2
+            list2 = list2.next
+        }
+
+        list3 = list3.next
+    }
+
+    list3.next = list1 || list2
+
+    return head.next
+};
