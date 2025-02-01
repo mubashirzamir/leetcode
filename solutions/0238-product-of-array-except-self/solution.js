61d55929ce7fac5a909cc4cdc100e5f77c71af94
Mubashir Zamir <79789067+mubashirzamir@users.noreply.github.com>
2025-02-01 07:37:12 +0000

    [LeetCode Sync] Runtime - 3 ms (93.50%), Memory - 62.6 MB (82.46%)

--- solutions/0238-product-of-array-except-self/solution.js
+++ solutions/0238-product-of-array-except-self/solution.js
@@ -3,24 +3,21 @@
  * @return {number[]}
  */
 var productExceptSelf = function (nums) {
-    const size = nums.length
-    let left = Array(size).fill(1)
-    let right = Array(size).fill(1)
-    let result = Array(size).fill(1)
+    let start1 = 1
+    let res = []
 
-    const help = (arr, index) => {
-        if (arr[index] === undefined) return 1
-        return arr[index]
+    for (let i = 0; i < nums.length; i++) {
+        res.push(start1)
+        start1 = start1 * nums[i]
     }
 
-    for (let i = 0; i < size; i++) {
-        left[i] = help(left, i - 1) * help(nums, i - 1)
-        right[size - 1 - i] = help(right, size - i) * help(nums, size - i)
+
+    let start2 = 1
+
+    for (let i = nums.length - 1; i >= 0; i--) {
+        res[i] = start2 * res[i]
+        start2 = start2 * nums[i]
     }
 
-    for (let i = 0; i < size; i++) {
-        result[i] = left[i] * right[i]
-    }
-
-    return result
+    return res
 };
