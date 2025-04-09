5d09fdf78c2bb96ca0ba9c9dd51df792b01f7f16
Mubashir Zamir <79789067+mubashirzamir@users.noreply.github.com>
2025-04-09 22:36:44 +0000

    [LeetCode Sync] Runtime - 9 ms (43.14%), Memory - 59.8 MB (29.41%)

--- solutions/0003-longest-substring-without-repeating-characters/solution.js
+++ solutions/0003-longest-substring-without-repeating-characters/solution.js
@@ -3,29 +3,23 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-    let seen = new Set()
-    let current = 0
-    let result = 0
     let lo = 0
     let hi = 0
+    let current = 0
+    let result = 0
+    let seen = new Set()
 
     while (hi < s.length) {
-        const c = s.charAt(hi)
-
-        if (seen.has(c)) {
-            while (s.charAt(lo) !== c) {
-                seen.delete(s.charAt(lo))
-                lo++
-                current--
-            }
-
+        while (seen.has(s[hi])) {
+            seen.delete(s[lo])
+            current--
             lo++
-        } else {
-            seen.add(c)
-            result = Math.max(result, ++current)
         }
 
+        seen.add(s[hi])
+        current++
         hi++
+        result = Math.max(result, current)
     }
 
     return result
