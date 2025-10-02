edd50236d14208d8dedf0df4ce1bffa6f3cfb1c2
Mubashir Zamir <79789067+mubashirzamir@users.noreply.github.com>
2025-10-02 21:36:44 +0000

    [LeetCode Sync] Runtime - 7 ms (27.57%), Memory - 59 MB (18.31%)

--- solutions/0169-majority-element/solution.js
+++ solutions/0169-majority-element/solution.js
@@ -3,11 +3,23 @@
  * @return {number}
  */
 var majorityElement = function (nums) {
+    // runtime constraints?
+    // space constraints?
     let map = new Map()
+    let result = { num: nums[0], instances: 1 }
 
-    for (n of nums) {
-        map.set(n, (map.get(n) || 0) + 1)
+    for (let n of nums) {
+        if (map.has(n)) {
+            m = map.get(n) + 1
+            map.set(n, m)
+            if (m > result.instances) {
+                result.num = n
+                result.instances = m
+            }
+        } else {
+            map.set(n, 1)
+        }
     }
 
-    return new Map([...map].sort((a, b) => b[1] - a[1])).keys().next().value
+    return result.num
 };
