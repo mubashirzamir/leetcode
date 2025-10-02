4abf638f4d551c1726131b582ed2df4deb3c1f89
Mubashir Zamir <79789067+mubashirzamir@users.noreply.github.com>
2025-10-02 21:47:21 +0000

    [LeetCode Sync] Runtime - 16 ms (78.20%), Memory - 73.4 MB (66.71%)

--- solutions/0217-contains-duplicate/solution.js
+++ solutions/0217-contains-duplicate/solution.js
@@ -3,11 +3,11 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    const set = new Set()
+    let set = new Set()
 
     for (n of nums) {
         if (set.has(n)) return true
-        set.add(n)
+        else set.add(n)
     }
 
     return false
