9fcfd936dba6b36009cb49e3552810f05de219e8
Mubashir Zamir <79789067+mubashirzamir@users.noreply.github.com>
2025-11-04 12:02:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 55.4 MB (40.96%)

---
+++ solutions/0908-middle-of-the-linked-list/solution.ts
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function middleNode(head: ListNode | null): ListNode | null {
+    // two pointers (fast & slow)
+    let fast = head
+    let slow = head
+
+    while (fast?.next) {
+        fast = fast?.next?.next
+        slow = slow.next
+    }
+
+    return slow
+};
