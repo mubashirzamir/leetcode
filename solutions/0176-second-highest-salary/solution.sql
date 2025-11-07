32db4a537775631ddcc8400f4147a2171416bcfe
Mubashir Zamir <79789067+mubashirzamir@users.noreply.github.com>
2025-11-07 21:21:18 +0000

    [LeetCode Sync] Runtime - 208 ms (45.90%), Memory - 0.0B (100.00%)

---
+++ solutions/0176-second-highest-salary/solution.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+SELECT COALESCE(
+    (
+        SELECT DISTINCT salary
+        FROM employee
+        ORDER BY salary DESC
+        LIMIT 1 OFFSET 1
+    ),
+    NULL
+) AS SecondHighestSalary;
