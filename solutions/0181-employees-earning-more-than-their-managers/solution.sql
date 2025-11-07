05a5531315b0c64e7a5bef373bb7422aaf2b43ed
Mubashir Zamir <79789067+mubashirzamir@users.noreply.github.com>
2025-11-07 20:11:39 +0000

    [LeetCode Sync] Runtime - 230 ms (64.72%), Memory - 0.0B (100.00%)

--- solutions/0181-employees-earning-more-than-their-managers/solution.sql
+++ solutions/0181-employees-earning-more-than-their-managers/solution.sql
@@ -1,5 +1,5 @@
-# Write your MySQL query statement below
-# subquery
-select name as Employee from Employee e where (
-    select salary from Employee where id = e.managerId
-) < salary
+-- Write your PostgreSQL query statement below
+select e.name as employee
+from employee e
+join employee m on m.id = e.managerId
+where m.salary < e.salary 
