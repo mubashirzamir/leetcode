10ed070ff060bb8e8f6cd28235f4d531ba2e8ce8
Mubashir Zamir <79789067+mubashirzamir@users.noreply.github.com>
2026-04-22 14:04:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 58.9 MB (34.06%)

--- solutions/0792-binary-search/solution.ts
+++ solutions/0792-binary-search/solution.ts
@@ -1,16 +1,14 @@
 function search(nums: number[], target: number): number {
     let lo = 0
-    let hi = nums.length - 1
+    let hi = nums.length
 
     while (lo <= hi) {
         const mid = Math.floor((lo + hi) / 2)
-        if (nums[mid] < target) {
-            lo = mid + 1
-        } else if (nums[mid] > target) {
-            hi = mid - 1
-        } else {
-            return mid
-        }
+
+        if (nums[mid] === target) return mid
+
+        if (target > nums[mid]) lo = mid + 1
+        else hi = mid -1
     }
 
     return -1
