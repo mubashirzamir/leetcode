9d8c5330b7d6d82d98e819a421b816d6a727e29f
Mubashir Zamir <79789067+mubashirzamir@users.noreply.github.com>
2026-04-22 15:43:53 +0000

    [LeetCode Sync] Runtime - 6 ms (8.92%), Memory - 59.6 MB (38.85%)

--- solutions/0733-flood-fill/solution.ts
+++ solutions/0733-flood-fill/solution.ts
@@ -1,38 +1,35 @@
 function floodFill(image: number[][], sr: number, sc: number, color: number): number[][] {
-    const newImage = Array.from({ length: image.length }, (_, i) =>
-        Array.from({ length: image[0].length }, (_, j) => image[i][j])
-    )
+    const queue = [[sr, sc]]
 
     const visited = Array.from({ length: image.length }, () =>
         Array.from({ length: image[0].length }, () => false)
     )
 
     const dirs = [
-        [0, 1],  // right
-        [0, -1], // left
-        [1, 0],  // down,
-        [-1, 0], // up
+        [0, 1],
+        [0, -1],
+        [1, 0],
+        [-1, 0]
     ]
 
-    const dfs = (i, j) => {
-        visited[i][j] = true
+    while (queue.length !== 0) {
+        const [i, j] = queue.shift()
+        const currColor = image[i][j]
 
         for (const d of dirs) {
             const newI = i + d[0]
             const newJ = j + d[1]
 
-            if (newI < 0 || newI > image.length - 1 || newJ < 0 || newJ > image[0].length - 1) continue
-            if (visited[newI][newJ]) continue
-
-            if (image[i][j] === image[newI][newJ]) {
-                newImage[newI][newJ] = color
-                dfs(newI, newJ)
-            }
+            // Out of bounds
+            if (newI < 0 || newI >= image.length || newJ < 0 || newJ >= image[0].length || visited[newI][newJ]) continue
+            // Push into queue
+            if (currColor === image[newI][newJ]) queue.push([newI, newJ])
         }
-    }
 
-    dfs(sr, sc)
-    newImage[sr][sc] = color
+        image[i][j] = color
+        visited[i][j] = true
+    }   
 
-    return newImage
+    return image
+
 };
