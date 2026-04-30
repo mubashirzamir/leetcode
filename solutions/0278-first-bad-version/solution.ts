735ad1f339f27a71851fb314545cc0127588c16a
Mubashir Zamir <79789067+mubashirzamir@users.noreply.github.com>
2026-04-30 09:22:48 +0000

    [LeetCode Sync] Runtime - 43 ms (61.79%), Memory - 55.7 MB (12.97%)

--- solutions/0278-first-bad-version/solution.ts
+++ solutions/0278-first-bad-version/solution.ts
@@ -5,18 +5,19 @@
  * };
  */
 
-var solution = function (isBadVersion: any) {
+var solution = function(isBadVersion: any) {
 
-    return function (n: number): number {
-        let lo = 1, hi = n
+    return function(n: number): number {
+        let hi = n
+        let lo = 0
 
         while (lo <= hi) {
-            const mid = Math.floor((lo + hi) / 2)
+            const mid = Math.trunc((lo + hi) / 2)
 
-            if (isBadVersion(mid)) {
-                if (mid - 1 <= 0 || !isBadVersion(mid - 1)) return mid
-                hi = mid - 1
-            } else lo = mid + 1
+            if (!isBadVersion(mid) && isBadVersion(mid + 1)) return mid + 1
+            if (isBadVersion(mid) && !isBadVersion(mid - 1)) return mid
+            else if (!isBadVersion(mid)) lo = mid + 1
+            else if (isBadVersion(mid)) hi = mid - 1
         }
     };
 };
