c0ff8639b3a24561f1749285772bb68cd3f881a3
Mubashir Zamir <79789067+mubashirzamir@users.noreply.github.com>
2026-05-02 08:41:29 +0000

    [LeetCode Sync] Runtime - 8 ms (11.26%), Memory - 58.2 MB (39.83%)

--- solutions/0409-longest-palindrome/solution.ts
+++ solutions/0409-longest-palindrome/solution.ts
@@ -1,33 +1,24 @@
 function longestPalindrome(s: string): number {
-    // Build count map
-    // Order by descending
-    // Use all even
-    // Use the highest which is odd
-    // For the rest of the odd occurences leave one behind
+    // To build the longest palindrome:
+    // 1. Have a count map
+    // 2. Have a usedOdd counter (there can only be one character that has an odd count in a palindrome)
+    // 3. Use the useOdd as soon as possible (Doesn't matter really, just to get it out of the way)
+
     let sMap = new Map<string, number>()
-    let result = 0
 
     for (const c of s) {
         sMap.set(c, (sMap.get(c) || 0) + 1)
     }
 
-    let arr = []
-    for (const [key, value] of sMap) {
-        arr.push([key, value])
-    }
+    let result = 0
+    let usedOdd = false
 
-    arr.sort((a, b) => b[1] - a[1])
-
-    let oddUsed = false
-
-    for (const item of arr) {
-        if (item[1] % 2 === 0) result += item[1]
-        else if (!oddUsed) {
-            result += item[1]
-            oddUsed = true
-        } else {
-            result += item[1] - 1
-        }
+    for (const [_, value] of sMap) {
+        if (!usedOdd && value % 2 !== 0) {
+            result += value
+            usedOdd = true
+        } else if (usedOdd && value % 2 !== 0) result += value - 1
+        else result += value
     }
 
     return result
