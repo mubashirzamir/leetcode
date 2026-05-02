3e9dc3471ae2a34474e9fc2ec122642e59a5517e
Mubashir Zamir <79789067+mubashirzamir@users.noreply.github.com>
2026-05-02 14:55:12 +0000

    [LeetCode Sync] Runtime - 5 ms (41.43%), Memory - 59.4 MB (41.18%)

--- solutions/0169-majority-element/solution.ts
+++ solutions/0169-majority-element/solution.ts
@@ -1,9 +1,9 @@
 function majorityElement(nums: number[]): number {
-    let map = new Map<number, number>()
+    const map = new Map<number, number>()
 
-    for (const n of nums) {
-        map.set(n, (map.get(n) || 0) + 1)
+    for (const c of nums) {
+        map.set(c, (map.get(c) || 0) + 1)
     }
 
-    return new Map([...map].sort((a, b) => b[1] - a[1])).keys().next().value
+    return [...map].sort((a, b) => b[1] - a[1])[0][0]
 };
