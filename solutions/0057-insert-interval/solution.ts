21a24f0fd5724420d3a22cb8172fed1a5cd54fb5
Mubashir Zamir <79789067+mubashirzamir@users.noreply.github.com>
2026-05-16 17:55:05 +0000

    [LeetCode Sync] Runtime - 3 ms (61.67%), Memory - 60 MB (21.11%)

--- solutions/0057-insert-interval/solution.ts
+++ solutions/0057-insert-interval/solution.ts
@@ -1,16 +1,34 @@
 function insert(intervals: number[][], newInterval: number[]): number[][] {
-    let result = []
+    const result: number[][] = [];
+    let i = 0;
 
-    for (let i = 0; i < intervals.length; i++) {
-        if (intervals[i][0] > newInterval[1]) {
-            return [...result, newInterval, ...intervals.slice(i, intervals.length)]
-        } else if (intervals[i][1] < newInterval[0]) {
-            result.push(intervals[i])
-        } else {
-            newInterval[0] = Math.min(newInterval[0], intervals[i][0])
-            newInterval[1] = Math.max(newInterval[1], intervals[i][1])
-        }
+    // 1. BEFORE newInterval
+    // [1,2] vs [5,7] -> no overlap, push
+    // next [3,6] vs [5,7] -> overlap, stop
+    while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+        result.push(intervals[i]);
+        i++;
     }
 
-    return [...result, newInterval]
-};
+    // 2. MERGE overlaps
+    // [3,6] vs [5,8] -> merge into [3,8]
+    // next [10,12] vs [3,8] -> no overlap, stop
+    while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+        newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+        newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+        i++;
+    }
+
+    // 3. PUSH merged interval
+    // merged interval = [3,8]
+    result.push(newInterval);
+
+    // 4. AFTER newInterval
+    // [10,12] vs [3,8] -> no overlap, push
+    while (i < intervals.length) {
+        result.push(intervals[i]);
+        i++;
+    }
+
+    return result;
+}
