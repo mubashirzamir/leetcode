ee0eb7cdc427666d7bfdaa2025a1dc96d8d63b25
Mubashir Zamir <79789067+mubashirzamir@users.noreply.github.com>
2026-05-18 14:48:13 +0000

    [LeetCode Sync] Runtime - 63 ms (46.37%), Memory - 74.5 MB (38.06%)

--- solutions/1014-k-closest-points-to-origin/solution.ts
+++ solutions/1014-k-closest-points-to-origin/solution.ts
@@ -1,7 +1,5 @@
 function kClosest(points: number[][], k: number): number[][] {
-    const withDistances = points.map(([x, y]) => {
-        return [x, y, Math.sqrt(Math.pow((x - 0), 2) + Math.pow((y - 0), 2))]
-    })
+    const withDistances = points.map(a => [a[0], a[1], Math.sqrt(Math.pow(a[0], 2) + Math.pow(a[1], 2))])
 
-    return withDistances.sort((a, b) => a[2] - b[2]).map(item => [item[0], item[1]]).slice(0, k)
+    return withDistances.sort((a, b) => a[2] - b[2]).map(x => [x[0], x[1]]).slice(0, k)
 };
