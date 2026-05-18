8adee14b6f0e4dd5c04533924f611801ff10edce
Mubashir Zamir <79789067+mubashirzamir@users.noreply.github.com>
2026-05-18 15:25:31 +0000

    [LeetCode Sync] Runtime - 134 ms (12.54%), Memory - 62 MB (19.86%)

--- solutions/0003-longest-substring-without-repeating-characters/solution.ts
+++ solutions/0003-longest-substring-without-repeating-characters/solution.ts
@@ -1,22 +1,24 @@
 function lengthOfLongestSubstring(s: string): number {
     // two pointers
-    // use a set to keep track of current characters
-    // right++ until no repeating characters
-    // left++ until set does not have duplicates 
-    const set = new Set<string>()
-    let max = 0, left = 0, right = 0
 
-    while (right <= s.length - 1) {
-        while (set.has(s.charAt(right))) {
-            set.delete(s.charAt(left))
+    let left = 0
+    let right = 0
+    let result = 0
+    let set = new Set<string>()
+
+    while (left <= right && right < s.length && result <= s.length - left) {
+        const c = s.charAt(right)
+
+        if (set.has(c)) {
             left++
+            right = left
+            set.clear()
+        } else {
+            set.add(c)
+            result = Math.max(result, right - left + 1)
+            right++
         }
-
-        set.add(s.charAt(right))
-        max = Math.max(max, set.size)
-
-        right++
     }
 
-    return max
+    return result
 };
