25bf701c4c4a82d81baf265509f89dcb160469df
Mubashir Zamir <79789067+mubashirzamir@users.noreply.github.com>
2026-05-20 18:26:22 +0000

    [LeetCode Sync] Runtime - 35 ms (64.11%), Memory - 71.2 MB (97.03%)

--- solutions/0015-3sum/solution.ts
+++ solutions/0015-3sum/solution.ts
@@ -1,36 +1,27 @@
 function threeSum(nums: number[]): number[][] {
-    // sort
-    // three pointers
-    // fix one pointer i
-    // j = i + 1, k = nums.length - 1
-    // converge until nums[i] + nums[j] + nums[k] !=== 0
-    nums = nums.sort((a, b) => a - b)
+    // Three pointer approach learned from AlgoJS
+
+    nums.sort((a, b) => a - b)
     let result = []
 
-    for (let i = 0; i < nums.length; i++) {
-        if (i > 0 && nums[i] === nums[i - 1]) continue // why
+    for (let i = 0; i < nums.length - 2; i++) {
+        // Avoid duplicates
+        if (i > 0 && nums[i] === nums[i - 1]) continue
 
         let j = i + 1
         let k = nums.length - 1
 
         while (j < k) {
-            const a = nums[i]
-            const b = nums[j]
-            const c = nums[k]
+            let sum = nums[i] + nums[j] + nums[k]
 
-            if (a + b + c === 0) {
-                result.push([a, b, c])
-                while (nums[j] === b) {
-                    j++
-                }
+            if (sum > 0) k--
+            else if (sum < 0) j++
+            else {
+                result.push([nums[i], nums[j], nums[k]])
 
-                while (nums[k] === c) {
-                    k--
-                }
-            } else if (a + b + c > 0) {
-                k--
-            } else {
-                j++
+                // Avoid duplicates
+                do { j++ } while (j < k && nums[j] === nums[j - 1])
+                do { k-- } while (k > j && nums[k] === nums[k + 1])
             }
         }
     }
