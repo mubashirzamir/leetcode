08f3b5f519723e0277c06424ba58dcb61bfb3b81
Mubashir Zamir <79789067+mubashirzamir@users.noreply.github.com>
2026-05-26 17:53:53 +0000

    [LeetCode Sync] Runtime - 13 ms (34.55%), Memory - 61.5 MB (33.06%)

--- solutions/1036-rotting-oranges/solution.ts
+++ solutions/1036-rotting-oranges/solution.ts
@@ -1,80 +1,44 @@
 function orangesRotting(grid: number[][]): number {
-    let rottenPositions = []
-    let visited = []
-    let fresh = 0
+    // BFS
+    // Every level of BFS is one minute
+    // Return number of levels
+
+    let queue = []
+    let ones = 0
+    let result = 0
 
     for (let i = 0; i < grid.length; i++) {
         for (let j = 0; j < grid[0].length; j++) {
-            if (visited[i] === undefined) visited[i] = []
-
-            visited[i][j] = {
-                rotTime: -1,
-                iteration: -1
-            }
-
-            let rotTime = -1
-            switch (grid[i][j]) {
-                case 0:
-                    rotTime = 0
-                    break
-                case 1:
-                    fresh++
-                    rotTime = Infinity
-                    break
-                case 2:
-                    rottenPositions.push([i, j])
-                    rotTime = 0
-                    break
-            }
-
-            visited[i][j].rotTime = rotTime
+            if (grid[i][j] === 2) queue.push([i, j, 0])
+            else if (grid[i][j] === 1) ones++
         }
     }
 
-    const dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-    let rotten = 0
+    const dirs = [
+        [0, 1],
+        [0, -1],
+        [1, 0],
+        [-1, 0]
+    ]
 
-    const bfs = (i, j, iteration) => {
-        let queue = [[i, j]]
+    while (queue.length !== 0) {
+        const [i, j, level] = queue.shift()
 
-        while (queue.length !== 0) {
-            const [p, q] = queue.shift()
-            visited[p][q].iteration = iteration
+        for (const [x, y] of dirs) {
+            const newI = i + x
+            const newJ = j + y
 
-            for (const d of dirs) {
-                const newI = p + d[0]
-                const newJ = q + d[1]
+            // Out of bounds
+            if (newI < 0 || newI >= grid.length || newJ < 0 || newJ >= grid[0].length) continue
+            if (grid[newI][newJ] === 0 || grid[newI][newJ] === 2) continue
 
-                // Skip out of bounds
-                if (newI < 0 || newI > grid.length - 1 || newJ < 0 || newJ > grid[0].length - 1) continue
-                // Skip empty cell
-                if (grid[newI][newJ] === 0) continue
-                // Skip visited
-                if (visited[newI][newJ].iteration === iteration) continue
-                // Skip rotten: BFS will handle them anyways
-                if (grid[newI][newJ] === 2) continue
-
-                if (visited[newI][newJ].rotTime === Infinity) rotten++
-                visited[newI][newJ].rotTime = Math.min(visited[newI][newJ].rotTime, visited[p][q].rotTime + 1)
-
-                queue.push([newI, newJ])
-            }
+            ones--
+            grid[newI][newJ] = 2
+            queue.push([newI, newJ, level + 1])
         }
+
+        if (queue.length === 0) result = level
     }
 
-    let iteration = 0
-    for (const [i, j] of rottenPositions) {
-        bfs(i, j, iteration++)
-    }
-
-    if (rotten < fresh) return -1
-
-    let result = -Infinity
-    for (let i = 0; i < visited.length; i++) {
-        for (let j = 0; j < visited[0].length; j++) {
-            result = Math.max(result, visited[i][j].rotTime)
-        }
-    }
-
-    return result
+    return ones === 0 ? result : -1
 };
