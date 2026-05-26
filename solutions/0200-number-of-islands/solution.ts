587f2b9100613060cb4c73cb70fea6f0b96bc70c
Mubashir Zamir <79789067+mubashirzamir@users.noreply.github.com>
2026-05-26 17:31:26 +0000

    [LeetCode Sync] Runtime - 114 ms (9.21%), Memory - 72.8 MB (5.00%)

--- solutions/0200-number-of-islands/solution.ts
+++ solutions/0200-number-of-islands/solution.ts
@@ -1,44 +1,40 @@
 function numIslands(grid: string[][]): number {
-    // Get position of all ones
-    // DFS in a loop, increment+ every time you come out of the loop until all ones are visited
+    // Multi-source DFS, increment result everytime we start a new source
 
-    const ones = []
+    let result = 0
+    let points = []
 
     for (let i = 0; i < grid.length; i++) {
         for (let j = 0; j < grid[0].length; j++) {
-            if (grid[i][j] === "1") ones.push([i, j])
+            if (grid[i][j] === '1') points.push([i, j])
         }
     }
 
-    const visited = Array.from({ length: grid.length }, () => (
-        Array.from({ length: grid[0].length }, () => false)
-    ))
+    const dirs = [
+        [1, 0],
+        [-1, 0],
+        [0, 1],
+        [0, -1]
+    ]
 
-    const dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-
-    const dfs = (i, j) => {
-        visited[i][j] = true
+    const dfs = (i: number, j: number) => {
+        grid[i][j] = '2'
 
         for (const [x, y] of dirs) {
-            const newI = x + i
-            const newJ = y + j
+            const newI = i + x
+            const newJ = j + y
 
-            // skip out of bounds
-            if (newI < 0 || newI > grid.length - 1 || newJ < 0 || newJ > grid[0].length - 1) continue
-            // skip visited
-            if (visited[newI][newJ]) continue
-            // skip zeroes
-            if (grid[newI][newJ] === "0") continue
-
-            dfs(newI, newJ)
+            // Out of bounds
+            if (newI < 0 || newI >= grid.length || newJ < 0 || newJ >= grid[0].length) continue
+            if (grid[newI][newJ] === '0' || grid[newI][newJ] === '2') continue
+            dfs(newI, newJ) 
         }
     }
 
-    let result = 0
+    for (const [x, y] of points) {
+        if (grid[x][y] === '2') continue
 
-    for (const [i, j] of ones) {
-        if (visited[i][j]) continue
-        dfs(i, j)
+        dfs(x, y)
         result++
     }
 
